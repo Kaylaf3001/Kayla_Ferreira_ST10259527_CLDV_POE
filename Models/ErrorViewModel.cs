51873d5ac8469cf755fec1544223190c71a47b63
Kayla <Kaylaf3001@gmail.com>
2024-03-11 08:24:14 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace POE_CloudDev.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
