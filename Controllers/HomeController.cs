e2e252510a7b2e9f313773115bd754e1ba04807c
Kayla <Kaylaf3001@gmail.com>
2024-04-10 11:51:20 +0200

    Added in the database for users, a login and sign up page with some basic layouts

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -32,6 +32,7 @@ namespace POE_CloudDev.Controllers
         { 
             return View();
         }
+        
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
---
+++ Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using POE_CloudDev.Models;
+
+namespace POE_CloudDev.Controllers
+{
+    public class UserController : Controller
+    {
+        public userTable usrtbl = new userTable();
+        public LoginModel loginModel = new LoginModel();
+
+
+        [HttpPost]
+        public ActionResult SignUp(userTable Users)
+        {
+            var result = usrtbl.insert_User(Users);
+            return RedirectToAction("MyWork", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult SignUp()
+        {
+            return View(usrtbl);
+        }
+
+
+        [HttpPost]
+        public ActionResult login(string email, string password)
+        {       
+            int userID = loginModel.SelectUser(email, password);
+            if (userID != -1)
+            {
+                // User found, proceed with login logic (e.g., set authentication cookie)
+                // For demonstration, redirecting to a dummy page
+                return RedirectToAction("Index", "Home", new { userID = userID });
+            }
+            else
+            {
+                // User not found, handle accordingly (e.g., show error message)
+                return View("LoginFailed");
+                
+            }
+        }
+        [HttpGet]
+        public ActionResult login()
+        {
+            return View(loginModel) ;
+        }
+    }
+
+
+}
+
